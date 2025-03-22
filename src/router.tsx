d08beccb0ac864a0401f45c87deb2fd58c567e30
gy <1340837581@qq.com>
2025-03-22 17:39:07 +0800

    更新路由

--- src/router.tsx
+++ src/router.tsx
@@ -2,8 +2,31 @@ import { RouteObject } from "react-router-dom";
 import Auth from "@/pages/auth";
 import Login from "@/pages/auth/login";
 import Register from "@/pages/auth/register";
+import Layout from "./pages/layout";
+import Home from "./pages/home";
+import Knowledge from "./pages/knowledge";
 
 const routes: RouteObject[] = [
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+    ],
+  },
+  {
+    path: "/wiki",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Knowledge />,
+      },
+    ],
+  },
   {
     path: "/auth",
     element: <Auth />,
