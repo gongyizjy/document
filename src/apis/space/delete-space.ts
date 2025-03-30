3ffbb29842f0e629930cdee76b7c54c7139dd62b
gy <1340837581@qq.com>
2025-03-30 18:51:15 +0800

    新增删除空间api

--- src/apis/space/delete-space.ts
+++ src/apis/space/delete-space.ts
@@ -1,5 +1,5 @@
 import instance from "../config";
 
 export default async function getSpaceDetail(spaceId: string) {
-  return await instance.get(`/api/spaces/${spaceId}`);
+  return await instance.post(`/api/spaces/${spaceId}`);
 }
