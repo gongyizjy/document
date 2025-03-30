c378b13fd2319e8c2dfc2ed904d27eba8ec41e81
gy <1340837581@qq.com>
2025-03-30 18:51:45 +0800

    新增获取空间下一级文档api

--- src/apis/docs/getSpaceFirstChild.ts
+++ src/apis/docs/getSpaceFirstChild.ts
@@ -4,8 +4,8 @@ import { TreeItemData } from "../space";
 
 export default async function getSpaceFirstChild(
   spaceId: string
-): Promise<Response<TreeItemData>> {
-  return await instance.get(`/api/doc`,{
+): Promise<Response<TreeItemData[]>> {
+  return await instance.get(`/api/doc/space`,{
     params: {
       spaceId,
     }
